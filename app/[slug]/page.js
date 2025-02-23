e635020a090d0c8bf1aa042e44d53eb2b49a14c5
Danil <litan.yt.terr@gmail.com>
2025-02-23 23:14:22 +0700

    Init back func

---
+++ app/[slug]/page.js
@@ -0,0 +1,34 @@
+'use client';
+import { useState } from "react";
+import Menu from "../components/Menu/Menu";
+import usePopState from "../utils/usePopState";
+import { useScrollHistory } from "../utils/useScrollHistory";
+import Link from "next/link";
+
+export default function Home() {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+
+  useScrollHistory();
+  usePopState((event) => {
+    const historyArray = JSON.parse(sessionStorage.getItem('scrollHistory')) || [];
+    const lastElement = historyArray[historyArray.length - 1];
+    if (lastElement === 'menu') {
+      setIsOpenMenu(false);
+      historyArray.pop()
+      sessionStorage.setItem('scrollHistory', JSON.stringify(historyArray));
+    } else if (lastElement === 'scroll') {
+      console.log('last scroll')
+      sessionStorage.setItem('isBlock', true);
+      historyArray.pop()
+      sessionStorage.setItem('scrollHistory', JSON.stringify(historyArray));
+      window.scrollTo(0, 0);
+    }
+  })
+
+  return (
+    <main className="h-[2200px] flex flex-col justify-center items-center bg-black">
+      <Menu isOpen={isOpenMenu} setIsOpen={setIsOpenMenu} />
+      <div className="p-4 bg-slate-600 text-white fixed bottom-2 left-1/4 cursor-pointer" onClick={() => window.history.back()}>back</div>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 'use client';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Menu from "./components/Menu/Menu";
 import usePopState from "./utils/usePopState";
 import { useScrollHistory } from "./utils/useScrollHistory";
+import Link from "next/link";
 
 export default function Home() {
   const [isOpenMenu, setIsOpenMenu] = useState(false);
@@ -27,6 +28,7 @@ export default function Home() {
   return (
     <main className="h-[2200px] flex flex-col justify-center items-center bg-black">
       <Menu isOpen={isOpenMenu} setIsOpen={setIsOpenMenu} />
+      <Link className="p-4 bg-slate-600 text-white fixed bottom-2 left-1/2 cursor-pointer" href='test'>GO test PAGE</Link>
       <div className="p-4 bg-slate-600 text-white fixed bottom-2 left-1/4 cursor-pointer" onClick={() => window.history.back()}>back</div>
     </main>
   );
